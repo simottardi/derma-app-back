8ccbd06f55889bc0070dd106751663e334dc7637
simottardi <simottardi@gmail.com>
2020-10-13 12:01:10 +0200

    completed models and migrations

---
+++ models/patientday.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class patientDay extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      patientDay.belongsTo(models.patient)
+      // define association here
+    }
+  };
+  patientDay.init({
+itchScore: {
+        type: DataTypes.INTEGER
+      },
+      note: {
+        type: DataTypes.STRING
+      },
+      image: {
+        type: DataTypes.STRING
+      },
+      medicationMorning: {
+        type: DataTypes.BOOLEAN
+      },
+      medicationAfternoon: {
+        type: DataTypes.BOOLEAN
+      },
+      medicationEvening: {
+        type: DataTypes.BOOLEAN
+      },
+
+       patientId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+
+
+      }
+  }, {
+    sequelize,
+    modelName: 'patientDay',
+  });
+  return patientDay;
+};
